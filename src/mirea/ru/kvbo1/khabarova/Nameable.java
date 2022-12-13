69cb4c932f8d226112c25fff5af2250bc2299532
Стася <103033198+anastaszias@users.noreply.github.com>
2022-12-13 13:02:35 +0300

    Add files via upload

---
+++ src/mirea/ru/kvbo1/khabarova/Nameable.java
@@ -0,0 +1,6 @@
+package mirea.ru.kvbo1.khabarova;
+
+public interface Nameable {
+    public String getName();
+
+}
---
+++ src/zadanie_4/prak_6/Priceable.java
@@ -0,0 +1,4 @@
+package zadanie_4.prak_6;
+public interface Priceable {
+    public double getPrice();
+}
